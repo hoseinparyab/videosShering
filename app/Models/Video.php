a345ccafb236914b145aa6a0383a4e7a2da562f0
hoseinparyab <75984205+hoseinparyab@users.noreply.github.com>
2024-06-21 23:00:59 +0330

    Convert date and time to Persian
    use the  package Verta

--- app/Models/Video.php
+++ app/Models/Video.php
@@ -2,10 +2,21 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Hekmatinasser\Verta\Verta;
 
 class Video extends Model
 {
     use HasFactory;
+
+
+   public function getLengthAttribute($value)
+   {
+        return gmdate("i:s",$value);
+   }
+   public function getCreatedAtAttribute($value)
+   {
+    return(new Verta($value))->formatDifference();
+   }
 }
