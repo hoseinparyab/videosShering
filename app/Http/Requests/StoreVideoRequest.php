a53981eea31991c683aef3d8ad55428867afba33
hoseinparyab <75984205+hoseinparyab@users.noreply.github.com>
2024-06-26 22:29:37 +0330

    Display previous request entries

--- app/Http/Requests/StoreVideoRequest.php
+++ app/Http/Requests/StoreVideoRequest.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Requests;
-
+use Illuminate\Support\Str;
 use Illuminate\Foundation\Http\FormRequest;
 
 class StoreVideoRequest extends FormRequest
@@ -26,10 +26,17 @@ class StoreVideoRequest extends FormRequest
         return [
             'name' => ['required'],
             'length' => ['required', 'integer'],
-            'slug' => ['required','unique:videos,slug'],
+            'slug' => ['required','unique:videos,slug','alpha_dash'],
             'url' => ['required','url'],
             'thumbnail' => ['required','url'],
             
         ];
+        
     }
+    protected function prepareForValidation()
+{
+    $this->merge([
+        'slug' => Str::slug($this->slug),
+    ]);
+}
 }
